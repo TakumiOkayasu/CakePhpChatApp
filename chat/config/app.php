d754fed2640900acb242b99682cc87a77691e3ba
TakumiOkayasu <t.okayasu@twb.jp>
2025-05-26 10:36:32 +0900

    feat: use env for debug log

--- chat/config/app.php
+++ chat/config/app.php
@@ -296,7 +296,7 @@ return [
              */
             'flags' => [],
             'cacheMetadata' => true,
-            'log' => false,
+            'log' => env('DEBUG', false),
 
             /*
              * Set identifier quoting to true if you are using reserved words or
